28739e5e020b681ace59bd62ed53b30d0c2ef108
pnarkiewicz <pawel123r@gmail.com>
2024-06-20 12:07:20 +0200

    stiffness_updated

--- src/eval_utils/metrics.py
+++ src/eval_utils/metrics.py
@@ -1,21 +1,23 @@
 import torch
 
 def stiffness(gradient_1, gradient_2, s_type="cosine"):
-    g_1 = gradient_1.reshape(-1)
-    g_2 = gradient_2.reshape(-1)
     if s_type == "cosine":
-        g_1 = g_1/torch.norm(g_1, p=2)
-        g_2 = g_2/torch.norm(g_2, p=2)
-        return torch.dot(g_1, g_2)/g_1.shape[0]
+        gradient_1 = gradient_1/(torch.norm(gradient_1, dim=-1, p=2).unsqueeze(-1) + 1e-6)
+        gradient_2 = gradient_2/(torch.norm(gradient_2, dim=-1, p=2).unsqueeze(-1) + 1e-6)
+        similarities = torch.einsum('bij,bkj->bik', gradient_1, gradient_2)
+        return similarities.mean()
     else:
-        return torch.sign(torch.dot(g_1, g_2))/g_1.shape[0]
+        similarities = torch.sign(torch.einsum('bij,bkj->bik', gradient_1, gradient_2))
+        return similarities.mean()
 
-def random_test_stiffness(N=10, M=15):
-    gradient_1 = torch.randn(N, M)
-    gradient_2 = torch.randn_like(gradient_1)
+def random_test_stiffness(B=4, N=10, M=12, S=768):
+    gradient_1 = torch.randn(B, N, S)
+    gradient_2 = torch.randn(B, M, S)
 
-    cosine_stiffness = stiffness(gradient_1, gradient_2)
+    cosine_stiffness = stiffness(gradient_1, gradient_2, s_type="cosine")
     sign_stiffness = stiffness(gradient_1, gradient_2, s_type="sign")
 
     assert len(cosine_stiffness.shape) == 0 and len(sign_stiffness.shape) == 0
     assert -1 <= cosine_stiffness <= 1, -1 <= sign_stiffness <= 1
+
+random_test_stiffness(B=4, N=10, M=12, S=768)
